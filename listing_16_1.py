5b358aad795e8fa30f5a2829b1544d652903a442
oliveiraphm <oliveiraphm@gmail.com>
2025-02-10 19:36:41 -0300

    ate pag 503

--- listing_16_1.py
+++ listing_16_1.py
@@ -14,6 +14,34 @@ X = enc.fit_transform(X)
 from pyod.models.auto_encoder import AutoEncoder
 
 np.random.seed(0)
-det = AutoEncoder(epochs=5, random_state=0)
+det = AutoEncoder(random_state=0)
 det.fit(X)
 pred = det.predict_proba(X)[:, 1]
+
+#listing_16_3.py
+from pyod.models.vae import VAE
+
+np.random.seed(0)
+det = VAE(random_state=0)
+det.fit(X)
+
+pred = det.predict_proba(X)[:, 1]
+
+#listing_16_4.py
+from pyod.models.alad import ALAD
+det = ALAD()
+det.fit(X)
+pred = det.decision_scores_
+det.plot_learning_curves()
+
+#listing_16_7.py
+from pyod.models.deep_svdd import DeepSVDD
+det = DeepSVDD()
+det.fit(X)
+pred = det.decision_scores_
+
+#listing_16_8.py
+from pyod.models.dif import DIF
+det = DIF()
+det.fit(X)
+pred = det.decision_scores_
